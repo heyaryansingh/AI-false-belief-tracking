5ede188fe75b2e223f5941af48983ebfee49e4da
Aryan Singh <aryanolympiads@gmail.com>
2026-01-16 22:54:11 -0600

    feat(03-02): create VirtualHome task programs library and episode generator

--- src/bsa/envs/virtualhome/__init__.py
+++ src/bsa/envs/virtualhome/__init__.py
@@ -2,8 +2,15 @@
 
 try:
     from .env import VirtualHomeEnvironment
+    from .tasks import get_task, list_tasks
+    from .episode_generator import VirtualHomeEpisodeGenerator
 
-    __all__ = ["VirtualHomeEnvironment"]
+    __all__ = [
+        "VirtualHomeEnvironment",
+        "get_task",
+        "list_tasks",
+        "VirtualHomeEpisodeGenerator",
+    ]
 except ImportError:
     # VirtualHome not installed - export empty
     __all__ = []
